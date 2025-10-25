383a362e4f5951a6a822a3a7253d74c01c322bff
diego <sigaran.diego7work@gmail.com>
2025-10-24 19:19:42 -0600

    Initialize Repo

---
+++ Web Scraper/Program.cs
@@ -0,0 +1,46 @@
+﻿public class Program {
+
+  public static async Task Main(string[] args) {
+
+	string baseUrl = "https://developer.chrome.com/";
+
+	await GetRobotsData(baseUrl);
+
+  }
+
+  public static async Task GetRobotsData(string baseUrl) {
+
+	string robotsFileUrl = baseUrl + "robots.txt";
+
+	HttpClient client = new HttpClient();
+	HttpResponseMessage response = await client.GetAsync(robotsFileUrl);
+	response.EnsureSuccessStatusCode();
+	string responseBody = await response.Content.ReadAsStringAsync();
+
+  }
+
+}
+
+public class HttpClientWrapper {
+
+  HttpClient _client;
+
+  public HttpClientWrapper () {
+	_client = new();
+  }
+
+  public async Task<string> GetRobotsData(string baseUrl) {
+
+    string robotsFileUrl = baseUrl + "robots.txt";
+
+    HttpResponseMessage response = await _client.GetAsync(robotsFileUrl);
+
+    response.EnsureSuccessStatusCode();
+
+    string responseBody = await response.Content.ReadAsStringAsync();
+
+    return responseBody;
+
+  }
+
+}
